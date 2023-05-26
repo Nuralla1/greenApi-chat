b02f41b76a094f945d4885044db5bd0fbb14d902
Nuralla1 <muzamelovn1@gmail.com>
2023-05-26 20:22:26 +0600

    feat: add check auth

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,10 +1,19 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import styles from "./Layout.module.css";
 import { SideBar } from "../../components";
 
 export const Layout = () => {
   const { container, background } = styles;
+  const navigate = useNavigate();
+  const idInstance = sessionStorage.getItem("idInstance");
+  const apiToken = sessionStorage.getItem("apiToken");
+
+  useEffect(() => {
+    if (!idInstance || !apiToken) {
+      navigate("/login");
+    }
+  });
 
   return (
     <>
